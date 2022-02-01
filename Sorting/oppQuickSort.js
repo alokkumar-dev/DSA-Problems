b27835cecc22e701d616c85a6eef6f771b46f7a2
alokkumar-dev <alokshakya2601@gmail.com>
2022-02-01 23:40:27 +0530

    Quick sort code for simplest way

---
+++ Sorting/oppQuickSort.js
@@ -0,0 +1,26 @@
+
+let arr = [2, 3, 1, 4, 5];
+console.log(oppQuickSort(arr).join(" "));
+
+
+function oppQuickSort(arr){
+    if(arr.length <=1){
+        return arr;
+    }
+    let pivot = Math.floor(Math.random()*arr.length);
+    let left_arr = [];
+    let right_arr = [];
+    
+    for(let i=0; i<arr.length; i++){
+        if(i==pivot){
+            continue;
+        }
+        else if(arr[i]< arr[pivot]){
+            left_arr.push(arr[i]);
+        }
+        else{
+            right_arr.push(arr[i]);
+        }
+    }
+    return [...oppQuickSort(right_arr), arr[pivot], ...oppQuickSort(left_arr)];
+}
