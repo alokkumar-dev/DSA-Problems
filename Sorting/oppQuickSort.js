467622de2f3f5172c48bff0cbd442fb169a7e569
alokkumar-dev <alokshakya2601@gmail.com>
2022-02-03 23:31:03 +0530

    modify files

--- Sorting/oppQuickSort.js
+++ Sorting/oppQuickSort.js
@@ -1,24 +1,24 @@
 
-let arr = [2, 3, 1, 4, 5,7,12,3,2,1];
+let arr = [2, 3, 1, 4, 5, 7, 3, 2, 1];
 console.log(oppQuickSort(arr).join(" "));
 
 
-function oppQuickSort(arr){
-    if(arr.length <=1){
+function oppQuickSort(arr) {
+    if (arr.length <= 1) {
         return arr;
     }
-    let pivot = Math.floor(Math.random()*arr.length);
+    let pivot = Math.floor(Math.random() * arr.length);
     let left_arr = [];
     let right_arr = [];
-    
-    for(let i=0; i<arr.length; i++){
-        if(i==pivot){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i == pivot) {
             continue;
         }
-        else if(arr[i]< arr[pivot]){
+        else if (arr[i] < arr[pivot]) {
             left_arr.push(arr[i]);
         }
-        else{
+        else {
             right_arr.push(arr[i]);
         }
     }
